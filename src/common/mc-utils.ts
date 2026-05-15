8d995f9ef6d2b52f444d6ad8363b4690be437b1e
fascinated <liam@fascinated.cc>
2026-05-15 07:11:11 +0100

    remove the cache

--- src/common/mc-utils.ts
+++ src/common/mc-utils.ts
@@ -1,9 +1,4 @@
 import McUtilsAPI from "mcutils-js-api";
 import { env } from "./env";
-import { isProduction } from "./utils";
 
-export const mcUtilsApi = new McUtilsAPI(env.NEXT_PUBLIC_API_URL, {
-  next: {
-    revalidate: isProduction() ? 60 : 0,
-  },
-});
+export const mcUtilsApi = new McUtilsAPI(env.NEXT_PUBLIC_API_URL);
