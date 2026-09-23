cbc26b5131137bc11f183fa8e1a222395c17cc14
fascinated <liam@fascinated.cc>
2026-09-23 21:17:45 +0100

    Upgrade frontend tooling dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 export default defineConfig([
-  {
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
 
+  {
     rules: {
       "@next/next/no-img-element": "off",
       "react-hooks/exhaustive-deps": "off",
